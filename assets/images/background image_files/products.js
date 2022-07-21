4fcaf617f51ad119995792d5a1b3fcdba98e9d94
AllenMoringa <allen.mathews@student.moringaschool.com>
2022-07-21 21:03:15 +0300

    add an image

---
+++ assets/images/background image_files/products.js
@@ -0,0 +1,573 @@
+var $products,
+
+
+$products = {
+    
+    styler: {
+		name     : 'styler',
+		tag      : 'Ecommerce',
+		img      : 'https://s3.envato.com/files/381758223/styler-logo.png',
+		url      : 'https://ninetheme.com/themes/styler/fashion/landing/',
+		purchase : 'https://1.envato.market/KeEYaA'
+	},
+	
+	wavo: {
+		name     : 'wavo',
+		tag      : 'Creative',
+		img      : 'https://s3.envato.com/files/288014523/Thumbnail.png',
+		url      : 'https://ninetheme.com/themes/wavolanding/wavo-landing-page/',
+		purchase : 'https://1.envato.market/90jO5'
+	},
+	
+	awam: {
+		name     : 'awam',
+		tag      : 'Creative',
+		img      : 'https://s3.envato.com/files/301832828/logo.png',
+		url      : 'https://ninetheme.com/themes/awam-landing/',
+		purchase : 'https://1.envato.market/QaAW6'
+	},
+	
+	exline : {
+		name     : 'exline',
+		tag      : 'Landing',
+		img      : 'https://s3.envato.com/files/157898395/preview_logo.png',
+		url      : 'https://ninetheme.com/themes/exline/intro/',
+		purchase : 'https://1.envato.market/X4gB3'
+	},
+	
+    agrikon: {
+		name     : 'agrikon',
+		tag      : 'Retail',
+		img      : 'https://s3.envato.com/files/316334043/agrikon-thumbnial.png',
+		url      : 'https://landing.ninetheme.com/agrikon/',
+		purchase : 'https://1.envato.market/J5D0v'
+	},
+	
+	greengrocery: {
+		name     : 'greengrocery',
+		tag      : 'Retail',
+		img      : 'https://s3.envato.com/files/316334043/agrikon-thumbnial.png',
+		url      : 'https://ninetheme.com/themes/greengrocery/',
+		purchase : 'https://1.envato.market/J5D0v'
+	},
+	
+    venam: {
+		name     : 'venam',
+		tag      : 'Ecommerce',
+		img      : 'https://s3.envato.com/files/316334043/agrikon-thumbnial.png',
+		url      : 'https://landing.ninetheme.com/venam/',
+		purchase : 'https://1.envato.market/mg2aR7'
+	},
+	
+	wixi: {
+		name     : 'wixi',
+		tag      : 'Personal',
+		img      : 'https://s3.envato.com/files/319083137/logo-wixi.jpg',
+		url      : 'https://landing.ninetheme.com/wixi/',
+		purchase : 'https://1.envato.market/db5PM'
+	},
+    
+	honshi: {
+		name     : 'honshi',
+		tag      : 'Creative',
+		img      : 'https://s3.envato.com/files/222519607/thumbnail.png',
+		url      : 'https://ninetheme.com/themes/honshi/',
+		purchase : 'https://1.envato.market/NGg1O'
+	},
+	
+	urip : {
+		name     : 'urip',
+		tag      : 'Landing',
+		img      : 'https://s3.envato.com/files/148391696/logo.png',
+		url      : 'https://ninetheme.com/themes/elementor/urip/intro',
+		purchase : 'https://1.envato.market/PzeMR'
+	},
+	
+    forester: {
+		name     : 'forester',
+		tag      : 'Creative',
+		img      : 'https://s3.envato.com/files/231118600/forester-logo.png',
+		url      : 'https://ninetheme.com/themes/forester/',
+		purchase : 'https://1.envato.market/GbMy6'
+	},
+	
+	proland : {
+		name     : 'proland',
+		tag      : 'Landing',
+		img      : 'https://s3.envato.com/files/183167531/80x80.png',
+		url      : 'https://ninetheme.com/themes/proland/landing/',
+		purchase : 'https://1.envato.market/dy2d2'
+	},
+	
+	foevis: {
+		name     : 'foevis',
+		tag      : 'Portfolio',
+		img      : 'https://s3.envato.com/files/218766427/logo.png',
+		url      : 'https://ninetheme.com/themes/foevis/',
+		purchase : 'https://1.envato.market/ZV20W'
+	},
+
+	conversi: {
+		name     : 'conversi',
+		tag      : 'Landing',
+		img      : 'https://s3.envato.com/files/218464535/thumbnail.png',
+		url      : 'https://ninetheme.com/themes/conversi/',
+		purchase : 'https://1.envato.market/X46zM'
+	},
+
+	agricom: {
+		name     : 'agricom',
+		tag      : 'Agriculture',
+		img      : 'https://s3.envato.com/files/221867044/thumb.png',
+		url      : 'https://ninetheme.com/themes/agricom/',
+		purchase : 'https://1.envato.market/L0Jea'
+	},
+
+	agro: {
+		name     : 'agro',
+		tag      : 'Agriculture',
+		img      : 'https://s3.envato.com/files/260004299/thumb.png',
+		url      : 'https://ninetheme.com/themes/agro/',
+		purchase : 'https://themeforest.net/item/agro-agriculture-organic-food-wordpress-theme/23274955'
+	},
+
+     decoraty: {
+		name     : 'decoraty',
+		tag      : 'Ecommerce',
+		img      : 'https://s3.envato.com/files/389595129/decoraty.png',
+		url      : 'https://ninetheme.com/themes/decoraty1/intro/',
+		purchase : 'https://1.envato.market/mgv7Ja'
+	},
+	
+	naturally : {
+		name     : 'naturally',
+		tag      : 'Agriculture',
+		img      : 'https://s3.envato.com/files/281588758/thumbnail.jpg',
+		url      : 'https://ninetheme.com/themes/naturally/intro/',
+		purchase : 'https://1.envato.market/y1gJG'
+	},
+	
+	
+    betakit : {
+		name     : 'betakit',
+		tag      : 'marketing',
+		img      : 'https://s3.envato.com/files/290770525/logo.png',
+		url      : 'https://ninetheme.com/themes/betakit/intro/',
+		purchase : 'https://1.envato.market/ZgmaQ'
+	},
+	
+	cursornt : {
+		name     : 'cursornt',
+		tag      : 'Business',
+		img      : 'https://s3.envato.com/files/253533923/thumbnail.png',
+		url      : 'https://ninetheme.com/themes/cursor/',
+		purchase : 'https://1.envato.market/jR97b'
+	},
+	
+    techland : {
+        name     : 'techland',
+        tag      : 'Saas',
+        img      : 'https://s3.envato.com/files/268206892/thumb.png',
+        url      : 'https://ninetheme.com/themes/techland/intro',
+        purchase : 'https://1.envato.market/2erP7'
+    },
+    
+     quadron : {
+        name     : 'quadron',
+        tag      : 'Business',
+        img      : 'https://s3.envato.com/files/277282858/thumb_2.png',
+        url      : 'https://ninetheme.com/themes/quadron/intro',
+        purchase : 'https://1.envato.market/rrd3Q'
+    },
+    
+     renegery : {
+        name     : 'renegery',
+        tag      : 'Business',
+        img      : 'https://s3.envato.com/files/280875174/thumb.png',
+        url      : 'https://ninetheme.com/themes/r-energy/intro',
+        purchase : 'https://1.envato.market/LL3zV'
+    }, 
+    
+    lumire : {
+        name     : 'lumire',
+        tag      : 'Business',
+        img      : 'https://s3.envato.com/files/263420337/thumbnail.png',
+        url      : 'https://ninetheme.com/landing/business-wordpress-website-lumire/',
+        purchase : 'https://1.envato.market/Zeer0'
+    }, 
+
+	ekme : {
+		name     : 'ekme',
+		tag      : 'Landing',
+		img      : 'https://s3.envato.com/files/254507818/ekme_80x80.png',
+		url      : 'https://ninetheme.com/themes/ekme/',
+		purchase : 'https://1.envato.market/xJgK3'
+	},
+
+	cryptoland : {
+		name     : 'cryptoland',
+		tag      : 'Bitcoin',
+		img      : 'https://s3.envato.com/files/256267283/thumb.png',
+		url      : 'https://ninetheme.com/themes/cryptoland/demo-page',
+		purchase : 'https://1.envato.market/KBMLz'
+	},
+
+	crypterium : {
+		name     : 'crypterium',
+		tag      : 'Bitcoin',
+		img      : 'https://s3.envato.com/files/249035465/thumb3.png',
+		url      : 'https://ninetheme.com/themes/crypterium/intro/',
+		purchase : 'https://1.envato.market/k4rKx'
+	},
+
+	landium : {
+		name     : 'landium',
+		tag      : 'Landing',
+		img      : 'https://s3.envato.com/files/215836716/thumb.png',
+		url      : 'https://ninetheme.com/themes/landium/intro/',
+		purchase : 'https://1.envato.market/bkLR6'
+	},
+
+	softing : {
+		name     : 'softing',
+		tag      : 'Technology',
+		img      : 'https://s3.envato.com/files/259533694/thumb.png',
+		url      : 'https://ninetheme.com/themes/softing/',
+		purchase : 'https://themeforest.net/item/softing-responsive-wordpress-software-landing-page-theme/23177965'
+	},
+
+	seorun : {
+		name     : 'seorun',
+		tag      : 'SEO - Marketing',
+		img      : 'https://s3.envato.com/files/260692510/thumb.png',
+		url      : 'https://ninetheme.com/landing/seorun-seo-agency-wordpress-theme/',
+		purchase : 'https://themeforest.net/item/seorun-digital-marketing-agency-wordpress-theme/23327907'
+	},
+
+	super_coach : {
+		name     : 'super coach',
+		tag      : 'Business',
+		img      : 'https://s3.envato.com/files/226434525/logo.png',
+		url      : 'https://ninetheme.com/themes/super-coach/',
+		purchase : 'https://1.envato.market/bkL4B'
+	},
+
+	antidote : {
+		name     : 'antidote ',
+		tag      : 'Business',
+		img      : 'https://s3.envato.com/files/221895902/thumbnail.png',
+		url      : 'https://ninetheme.com/themes/antidote/',
+		purchase : 'https://1.envato.market/vDXGA'
+	},
+
+	grandium : {
+		name     : 'grandium',
+		tag      : 'Hotel',
+		img      : 'https://s3.envato.com/files/187366785/thumbnail.jpg',
+		url      : 'https://ninetheme.com/themes/grandium/',
+		purchase : 'https://themeforest.net/item/grandium-responsive-hotel-template/15793868'
+	},
+
+	quarto : {
+		name     : 'quarto',
+		tag      : 'Hotel',
+		img      : 'https://s3.envato.com/files/259709614/80x80.png',
+		url      : 'https://ninetheme.com/landing/the-quarto-online-booking-hotel-theme/',
+		purchase : 'https://themeforest.net/item/the-quarto-responsive-modern-wordpress-hotel-theme/23240803'
+	},
+
+	saulsattorneys: {
+		name     : 'saulsattorneys',
+		tag      : 'Lawyers',
+		img      : 'https://s3.envato.com/files/221271119/thumb_2.png',
+		url      : 'https://ninetheme.com/themes/saulsattorneys/',
+		purchase : 'https://1.envato.market/QYBVY'
+	},
+
+	ahope: {
+		name     : 'ahope',
+		tag      : 'Charity',
+		img      : 'https://s3.envato.com/files/225215675/thumbnail.png',
+		url      : 'https://ninetheme.com/themes/ahope/',
+		purchase : 'https://1.envato.market/2RObD'
+	},
+
+	sawmill : {
+		name     : 'sawmill',
+		tag      : 'Landing',
+		img      : 'https://s3.envato.com/files/201827704/thumbnail.png',
+		url      : 'https://ninetheme.com/themes/sawmill/',
+		purchase : 'https://1.envato.market/VOJXj'
+	},
+
+	advent : {
+		name     : 'advent',
+		tag      : 'Tecnology',
+		img      : 'https://s3.envato.com/files/215412858/logo.png',
+		url      : 'https://ninetheme.com/themes/advent/intro',
+		purchase : 'https://1.envato.market/vDXoN'
+	},
+
+	revity: {
+		name     : 'revity',
+		tag      : 'Multipurpose',
+		img      : 'https://s3.envato.com/files/221539241/thumbnail-Revity.png',
+		url      : 'https://ninetheme.com/landing/revity-creative-portfolio-parallax-wordpress-theme/',
+		purchase : 'https://1.envato.market/jRPKn'
+	},
+
+	metropol: {
+		name     : 'metropol',
+		tag      : 'Business',
+		img      : 'https://s3.envato.com/files/222167734/thumbnail.jpg',
+		url      : 'https://ninetheme.com/themes/metropol',
+		purchase : 'https://1.envato.market/KBVaa'
+	},
+
+	big_border_wp : {
+		name     : 'BIG Border',
+		tag      : 'Onepage',
+		img      : 'https://s3.envato.com/files/206176376/theme-logo-new.png',
+		url      : 'https://ninetheme.com/themes/big-border-wp/intro',
+		purchase : 'https://1.envato.market/MKZ0N'
+	},
+
+	wedpress : {
+		name     : 'wedpress',
+		tag      : 'Landing',
+		img      : 'https://s3.envato.com/files/255977205/thumb.jpg',
+		url      : 'https://ninetheme.com/themes/wedpress/intro/',
+		purchase : 'https://1.envato.market/n45KR'
+	},
+	company : {
+		name     : 'company',
+		tag      : 'Business',
+		img      : 'https://s3.envato.com/files/200563133/theme-logo.png',
+		url      : 'https://ninetheme.com/themes/company/',
+		purchase : 'https://1.envato.market/B5NLJ'
+	},
+	eventex : {
+		name     : 'eventex',
+		tag      : 'Event',
+		img      : 'https://s3.envato.com/files/256309249/eventex.png',
+		url      : 'https://ninetheme.com/themes/eventex/',
+		purchase : 'https://1.envato.market/B5v30'
+	},
+
+	blue: {
+		name     : 'blue',
+		tag      : 'Landing',
+		img      : 'https://s3.envato.com/files/220269810/thumbnail.png',
+		url      : 'https://ninetheme.com/themes/blue/',
+		purchase : 'https://1.envato.market/k45LM'
+	},
+
+	appx: {
+		name     : 'appx',
+		tag      : 'App+Landing',
+		img      : 'https://s3.envato.com/files/219387182/appx_logo.png',
+		url      : 'https://ninetheme.com/themes/appx/',
+		purchase : 'https://1.envato.market/5kROD'
+	},
+
+	promote: {
+		name     : 'promote',
+		tag      : 'Marketing',
+		img      : 'https://s3.envato.com/files/219118467/logo.png',
+		url      : 'https://ninetheme.com/themes/promote/',
+		purchase : 'https://1.envato.market/aNy3Q'
+	},
+
+	antique: {
+		name     : 'antique',
+		tag      : 'Multipurpose',
+		img      : 'https://s3.envato.com/files/219080552/antique_logo.png',
+		url      : 'https://ninetheme.com/themes/antique/',
+		purchase : 'https://1.envato.market/1G6EB'
+	},
+
+	probiz: {
+		name     : 'probiz',
+		tag      : 'Portfolio',
+		img      : 'https://s3.envato.com/files/220020223/logo.png',
+		url      : 'https://ninetheme.com/themes/probiz/',
+		purchase : 'https://1.envato.market/DW5Dq'
+	},
+
+	minfox : {
+		name     : 'minfox',
+		tag      : 'Landing',
+		img      : 'https://s3.envato.com/files/217295449/80x80.png',
+		url      : 'https://ninetheme.com/themes/minfox/',
+		purchase : 'https://1.envato.market/2ROnG'
+	},
+
+	violetta : {
+		name     : 'violetta',
+		tag      : 'Creative',
+		img      : 'https://s3.envato.com/files/214208566/violetta_thumb.png',
+		url      : 'https://ninetheme.com/themes/violetta',
+		purchase : 'https://1.envato.market/NGgnN'
+	},
+
+	acele : {
+		name     : 'acele',
+		tag      : 'Landing',
+		img      : 'https://s3.envato.com/files/210668110/00_thumbnail.jpg',
+		url      : 'https://ninetheme.com/themes/acele/intro',
+		purchase : 'https://1.envato.market/qWQdg'
+	},
+
+	campfire : {
+		name     : 'campfire',
+		tag      : 'Landing',
+		img      : 'https://s3.envato.com/files/207324133/theme-logo-new.png',
+		url      : 'https://ninetheme.com/themes/campfire/',
+		purchase : 'https://1.envato.market/Agd6a'
+	},
+
+	inview : {
+		name     : 'inview',
+		tag      : 'Photography',
+		img      : 'https://s3.envato.com/files/197841281/thumbnail.jpg',
+		url      : 'https://ninetheme.com/themes/inview/',
+		purchase : 'https://1.envato.market/bk6dM'
+	},
+
+	amaze : {
+		name     : 'amaze',
+		tag      : 'Onepage',
+		img      : 'https://s3.envato.com/files/205824241/theme-logo.png',
+		url      : 'https://ninetheme.com/themes/amaze/',
+		purchase : 'https://1.envato.market/2RomA'
+	},
+
+	mimian : {
+		name     : 'mimian',
+		tag      : 'Business',
+		img      : 'https://s3.envato.com/files/183312536/80x80.png',
+		url      : 'https://ninetheme.com/themes/mimian/',
+		purchase : 'https://1.envato.market/vD5dL'
+	},
+
+	architekt : {
+		name     : 'architekt',
+		tag      : 'Architecture',
+		img      : 'https://s3.envato.com/files/177211430/thumb.jpg',
+		url      : 'https://ninetheme.com/themes/architekt/',
+		purchase : 'https://1.envato.market/KBrbe'
+	},
+
+	bobby : {
+		name     : 'bobby',
+		tag      : 'Onepage',
+		img      : 'https://s3.envato.com/files/168851347/theme-logo.png',
+		url      : 'https://paul-themes.com/html/bobby/demo/',
+		purchase : 'https://1.envato.market/oNxMW'
+	},
+
+	xeon: {
+		name     : 'xeon',
+		tag      : 'Portfolio',
+		img      : 'https://s3.envato.com/files/219743755/logo.png',
+		url      : 'https://ninetheme.com/themes/xeon/',
+		purchase : 'https://1.envato.market/rEVjB'
+	},
+
+	meder: {
+		name     : 'meder',
+		tag      : 'WP',
+		img      : 'https://s3.envato.com/files/218873924/logo.png',
+		url      : 'https://ninetheme.com/themes/meder/',
+		purchase : 'https://1.envato.market/zzW17'
+	},
+	
+	beard : {
+		name     : 'beard',
+		tag      : 'Onepage',
+		img      : 'https://s3.envato.com/files/171248847/theme-logo.png',
+		url      : 'https://ninetheme.com/themes/beard',
+		purchase : 'https://1.envato.market/GbxW2'
+	},
+
+	holywood : {
+		name     : 'holywood',
+		tag      : 'Landing',
+		img      : 'https://s3.envato.com/files/168851880/theme-logo.png',
+		url      : 'https://ninetheme.com/themes/holywood',
+		purchase : 'https://1.envato.market/vD5ML'
+	},
+
+	cobian : {
+		name     : 'cobian',
+		tag      : 'landing',
+		img      : 'https://s3.envato.com/files/168851381/theme-logo.png',
+		url      : 'https://99webpage.com/theme-review/landingpage/cobian/demo.html',
+		purchase : 'https://1.envato.market/OYkqP'
+	},
+
+	church : {
+		name     : 'church',
+		tag      : 'WP',
+		img      : 'https://s3.envato.com/files/130402802/logo.png',
+		url      : 'https://ninetheme.com/themes/church',
+		purchase : 'https://1.envato.market/k4yrV'
+	},
+
+	confidence : {
+		name     : 'confidence',
+		tag      : 'Nonprofit',
+		img      : 'https://s3.envato.com/files/155562088/logo.png',
+		url      : 'https://ninetheme.com/themes/confidence/',
+		purchase : 'https://1.envato.market/QYaD6'
+	},
+
+	zakat : {
+		name     : 'zakat',
+		tag      : 'Nonprofit',
+		img      : 'https://s3.envato.com/files/120783792/logo.jpg',
+		url      : 'https://ninetheme.com/themes/zakat',
+		purchase : 'https://1.envato.market/L0jMo'
+	},
+
+	commerca_html : {
+		name     : 'commerca html',
+		tag      : 'html',
+		img      : 'https://s3.envato.com/files/86391530/logo.png',
+		url      : 'https://ninetheme.com/themes/html-templates/commerca',
+		purchase : 'https://themeforest.net/item/commerca-responsive-bootstrap-ecommerce-template/7299949?ref=Ninetheme'
+	},
+
+	big_border_html : {
+		name     : 'big border html',
+		tag      : 'html',
+		img      : 'https://s3.envato.com/files/204475119/theme-logo-new.png',
+		url      : 'https://ninetheme.com/themes/html-templates/big-border-html/', 
+		purchase : 'https://themeforest.net/item/responsive-bootstrap-html5-template-big-border/17438279?ref=Ninetheme'
+	},
+
+	zenda_html : {
+		name     : 'zenda html',
+		tag      : 'html',
+		img      : 'https://s3.envato.com/files/179066434/theme-logo.png',
+		url      : 'https://ninetheme.com/themes/html-templates/zenda/index.html',
+		purchase : 'https://1.envato.market/qWQMg'
+	}, 
+
+	serenity_html : {
+		name     : 'serenity html',
+		tag      : 'html',
+		img      : 'https://s3.envato.com/files/79550335/logo.png',
+		url      : 'https://ninetheme.com/themes/html-templates/serenity/index.html',
+		purchase : 'https://themeforest.net/item/serenity-minimal-responsive-ecommerce-template/4541605?ref=Ninetheme'
+	},
+
+	tender_html : {
+		name     : 'tender html',
+		tag      : 'html',
+		img      : 'https://s3.envato.com/files/95938574/logo.png',
+		url      : 'https://ninetheme.com/themes/html-templates/tender/index.html',
+		purchase : 'https://themeforest.net/item/serenity-minimal-responsive-ecommerce-template/4541605?ref=Ninetheme'
+	},
+
+};
